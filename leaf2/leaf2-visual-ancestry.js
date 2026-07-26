d1a47a46af59dc93465124a07eee722b26e9be5f
amuletmaiden <103682843+amuletmaiden@users.noreply.github.com>
2026-07-25 17:56:33 -0700

    Capture source Leaf renderers for leaf2

---
+++ leaf2/leaf2-visual-ancestry.js
@@ -0,0 +1,8 @@
+/* Capture the actual Leaf renderers before any Leaf² experiment replaces them. */
+(function(){
+'use strict';
+globalThis.LEAF2_ANCESTRAL_VISUALS={
+  drawHeartBody,drawHeart,drawLawSkirt,drawLoveSkirt,drawLove,drawLawGoddess,
+  drawTemple,drawGyreTrails,drawGyres,drawStars,drawRefraction
+};
+})();
