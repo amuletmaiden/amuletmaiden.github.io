cc8c88fccfe24612c1fc89a87901a501c412210d
amuletmaiden <103682843+amuletmaiden@users.noreply.github.com>
2026-08-09 10:34:55 -0700

    feat(greyblue): boot known landmark revisit surface

--- greyblue-archipelago/src/core/optional-surfaces-bootstrap.js
+++ greyblue-archipelago/src/core/optional-surfaces-bootstrap.js
@@ -19,6 +19,7 @@ const OPTIONAL_SURFACES = [
   ["known-crossing-destination-atmosphere", () => import("./known-crossing-destination-atmosphere-bootstrap.js")],
   ["known-crossing-destination-atmosphere-presentation", () => import("./known-crossing-destination-atmosphere-presentation-bootstrap.js")],
   ["roost-rest", () => import("./roost-rest-bootstrap.js")],
+  ["known-landmark-revisit", () => import("./known-landmark-revisit-bootstrap.js")],
   ["contextual-hud", () => import("../interface/contextual-hud-bootstrap.js")],
 ].map(([id, load]) => ({ id, load }));
 
