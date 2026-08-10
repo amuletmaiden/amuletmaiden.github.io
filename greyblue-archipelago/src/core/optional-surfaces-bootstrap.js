1788b7d0ed0cbaec6c18922310ae764c8532a56c
amuletmaiden <103682843+amuletmaiden@users.noreply.github.com>
2026-08-10 10:36:34 -0700

    Greyblue: load regional mystery search flight surface

--- greyblue-archipelago/src/core/optional-surfaces-bootstrap.js
+++ greyblue-archipelago/src/core/optional-surfaces-bootstrap.js
@@ -15,6 +15,7 @@ const OPTIONAL_SURFACES = [
   ["familiar-mist", () => import("./familiar-mist-bootstrap.js")],
   ["regional-omens", () => import("./regional-omen-bootstrap.js")],
   ["regional-mystery-thread", () => import("./regional-mystery-thread-bootstrap.js")],
+  ["regional-mystery-search-flight", () => import("./regional-mystery-search-flight-bootstrap.js")],
   ["expedition", () => import("./expedition-bootstrap.js")],
   ["familiar-crossing-signature", () => import("./familiar-crossing-signature-bootstrap.js")],
   ["known-crossing-destination-atmosphere", () => import("./known-crossing-destination-atmosphere-bootstrap.js")],
